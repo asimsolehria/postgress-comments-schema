38f8abfe51873555bcf6e798f5f60fa3e44079a6
Asim Habib <de.asimhabib@gmail.com>
2022-06-07 05:10:59 +0500

    Query solution updated

--- reply-insertion query.sql
+++ reply-insertion query.sql
@@ -1,6 +1,8 @@
-with rows as (
-INSERT INTO public.comments (title,comment_text) VALUES ('another reply','I am again replying to your comment') RETURNING *
-)
- INSERT INTO public.replyto(
- 	"commentId", title, replytext)
- 	SELECT id,title,comment_text FROM rows
+ 	with rows as (
+INSERT INTO public.replyto(
+	 "commentId", title, replytext)
+	VALUES (1, 'reply', 'I am replying to your comment') RETURNING "responseId","commentId"
+ )
+UPDATE public.comments
+SET "responseId" =  rows."responseId" FROM rows
+WHERE id=rows."commentId" 
